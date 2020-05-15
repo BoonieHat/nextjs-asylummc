8001fc6ce8dccc311fee97b2569c0d7fd9afd114
BoonieHat <38331628+BoonieHat@users.noreply.github.com>
2020-05-15 06:06:11 -0400

    Remove MyAsylum.. force of habit

--- components/Head.js
+++ components/Head.js
@@ -4,7 +4,7 @@ export default (props) => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
     <meta name="description" content={props.description}></meta>
-    <title>MyAsylum | {props.title}</title>
+    <title>MC Asylum | {props.title}</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;900&display=swap" rel="stylesheet"></link>
     <link rel="manifest" href="/manifest.json"></link>
   </Head>
